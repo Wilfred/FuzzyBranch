ebcc7e4f9ac225013cd4dae71d5f6966b0e9f511
Wilfred Hughes <me@wilfred.me.uk>
2012-09-13 15:53:51 +0100

    implementing trackingBranches, which ignores remote branches that we have locally

--- FuzzyBranch.hs
+++ FuzzyBranch.hs
@@ -1,8 +1,10 @@
 import Data.List.Split
 import Data.String.Utils -- from MissingH
 import Data.Maybe
+import Data.Monoid
 
-data Branch = LocalBranch String | RemoteBranch String deriving Show
+
+data Branch = LocalBranch String | RemoteBranch String deriving (Show, Eq)
 
 main = putStrLn "Hello, World!"
 
@@ -13,8 +15,20 @@ getAllBranches filePath = do
   let branchNames = map (dropWhile (/= '\t')) fileLines
   let branches = mapMaybe parseBranchName branchNames
   return branches
+
+-- a list of local branches and only the remote branches that don't
+-- have corresponding local branches
+trackingBranches :: [Branch] -> [Branch]
+trackingBranches [] = []
+trackingBranches branches = 
+  let localNames = [LocalBranch n | LocalBranch n <- branches]
+      remoteOnlyNames = [RemoteBranch n | RemoteBranch n <- branches, not $ (LocalBranch n) `elem` localNames]
+  in
+   mappend localNames remoteOnlyNames
   
-getWebBranches = getAllBranches "/home/wilfred/work/web/.git/info/refs"
+getWebBranches = do
+  allBranches <- getAllBranches "/home/wilfred/work/web/.git/info/refs"
+  return $ trackingBranches allBranches
 
 -- we ignore tags
 parseBranchName :: String -> Maybe Branch
